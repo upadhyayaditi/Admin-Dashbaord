c24ad2736c4a9354fda06fcc1b48ced03ab3b278
upadhyayaditi <au.aditi08@gmail.com>
2024-07-20 23:33:14 +0530

    Layout component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Dashboard/Dashboard"
+import Layout from "./components/Layout/Layout"
 
 
 function App() {
