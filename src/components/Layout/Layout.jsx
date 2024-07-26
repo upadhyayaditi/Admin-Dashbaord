ef061f8e697d0085c049d5b494281a32a1fdbf48
upadhyayaditi <au.aditi08@gmail.com>
2024-07-26 22:19:50 +0530

    rendering dashboard

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,11 +2,17 @@ import moment from 'moment/moment'
 import css from './Layout.module.css'
 import  { BiSearch } from 'react-icons/bi'
 import Sidebar from '../Sidebar/Sidebar'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
 const Layout = () => {
+
+    const {pathname} = useLocation()
+
   return (
     <div className={css.container}>
         <Sidebar />
+
+        {pathname === "/" && <Navigate to="/dashboard" />}
         <div className={css.dashboard}>
             <div className={css.topBaseGradients}>
                 <div className='gradient-red'></div>
@@ -28,7 +34,11 @@ const Layout = () => {
                         <span>devissteven@gmail.com</span>
                     </div>
                 </div>
+
             </div>
+                <div className={css.content}>
+                    <Outlet />
+                </div>
         </div> 
     </div>
   )
