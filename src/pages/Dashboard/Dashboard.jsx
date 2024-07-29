e051758d8299179d786014acd94875e50d660914
upadhyayaditi <au.aditi08@gmail.com>
2024-07-29 23:28:51 +0530

    head and options of the dashboard is completed

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,8 +2,30 @@ import css from './Dashboard.module.css'
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className={css.container}>
+      <div className={css.dashboard}>
+        <div className={css.dashboardHead}>
+          <div className={css.head}>
+            <span>Dashboard</span>
+
+            <div className={css.durationButton}>
+              <select>
+                <option value="">1 week</option>
+                <option value="">1 month</option>
+                <option value="">1 year</option>
+              </select>
+            </div>
+
+            <div className={css.cards}>
+              cards
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={css.orders}>
+        orders
+      </div>
     </div>
   )
 }
